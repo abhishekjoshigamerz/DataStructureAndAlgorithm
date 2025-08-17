24228d28fc2f47a54240d30f0aec36a30979249c
ABHISHEK\abhis <abhishekjoshicctian1@gmail.com>
2025-08-18 04:11:11 +0530

    First Linekd List program

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+
+    int data;
+    Node *node;
+
+    Node(int data1, Node *next)
+    {
+        data = data1;
+        node = next;
+    }
+};
+
+int main()
+{
+
+    vector<int> arr = {2, 5, 8, 7};
+    Node *y1 = new Node(arr[0], nullptr);
+    Node *y2 = new Node(arr[1], nullptr);
+    Node *y3 = new Node(arr[2], nullptr);
+    Node *y4 = new Node(arr[3], nullptr);
+    y1->node = y2;
+    y2->node = y3;
+    y3->node = y4;
+    cout << y1->data << " " << y1->node << "\n";
+    cout << y2->data << " " << y2->node << "\n";
+    cout << y3->data << " " << y3->node << "\n";
+    cout << y4->data << " " << y4->node << "\n";
+    return 0;
+}
